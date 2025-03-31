8249d0849727d42859842b6a948f05be7e188b56
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-03-31 11:58:34 +0100

    chore(eslint): unocss enabled

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import { ryoppippi } from '@ryoppippi/eslint-config/tanstack-start';
 export default ryoppippi({
 	type: 'app', // or 'lib'
 	tailwindcss: false,
+	unocss: true,
 	appDirectory: './app',
 	typescript: {
 		tsconfigPath: './tsconfig.json',
