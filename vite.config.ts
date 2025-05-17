357485120cbeb9b2fc4f3d509411bb58208f826d
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-05-17 14:06:09 +0100

    feat: migrate to tanstack-start alpha

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+	server: {
+		port: 3000,
+	},
 	plugins: [
 		tsConfigPaths({
 			projects: ['./tsconfig.json'],
