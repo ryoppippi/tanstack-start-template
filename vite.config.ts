efcf289341ec1db680b1cc6f286a8b1ba29d362e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-05-17 13:53:52 +0100

    feat: merge app/vitest -> vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import UnoCSS from 'unocss/vite';
+import { defineConfig } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+	plugins: [
+		tsConfigPaths({
+			projects: ['./tsconfig.json'],
+		}),
+		UnoCSS(),
+		tanstackStart({
+			tsr: {
+				indexToken: 'page',
+				routeToken: 'layout',
+			},
+			react: {
+				babel: {
+					plugins: [
+						[
+							'babel-plugin-react-compiler',
+							{
+								target: '19',
+							},
+						],
+					],
+				},
+			},
+		}),
+	],
+	test: {
+		globals: true,
+		includeSource: ['app/**/*.{js,ts,tsx}'],
+		setupFiles: ['vitest.setup.ts'],
+	},
+});
